b349f94dad2ce733be6acdc9b7078f7abef88000
Valentyna Orel <orelvalenti@gmail.com>
2021-01-16 09:14:35 +0100

    Add processOrder method to Product

--- src/js/script.js
+++ src/js/script.js
@@ -61,6 +61,8 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
       console.log('new Product: ', thisProduct);
     }
@@ -86,7 +88,11 @@
 
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      console.log('thisProduct.form: ', thisProduct.form);
+
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      console.log('thisProduct.formInputs: ', thisProduct.formInputs);
+
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
     }
@@ -117,6 +123,78 @@
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
+    }
+
+    initOrderForm(){
+      const thisProduct = this;
+      console.log('initOrderForm');
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder(){
+      const thisProduct = this;
+      console.log('processOrder');
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formaData', formData);
+
+      //set price to default price
+      let price = thisProduct.data.price;
+      const allParams = thisProduct.data.params;
+
+      //for every category (param)...
+      for(let paramId in allParams) {
+
+        //determine param value, e.g. paramId = 'toppings', param =  {label: 'Toppings', type: 'chackboxes'...}
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        //for every option in this category
+        for(let optionId in param.options) {
+
+          //determine option value, e.g. optionId = 'olives', option = {label: 'Olives', price: 2, default: true}
+          const option = param.options[optionId];
+          console.log(optionId, option);
+          //const paramOptions = allParams[param].options;
+          //const ingrPrice = paramOptions[options].price;
+          //console.log('ingrPrice', ingrPrice);
+
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+            // check if the option is not default
+            if(!option.default) {
+            // add option price to price variable
+            price += option.price;
+            }
+            } else {
+            // check if the option is default
+            if(option.default) {
+            // reduce price variable
+            price -= option.price;
+
+            }
+          }
+        }
+      }
+
+      //update calculated price in HTML
+      thisProduct.priceElem.innerHTML = price;
+
     }
   }
 
