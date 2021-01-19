3def243df0cca5cdb4f423aee0dcb816f72af1e6
Valentyna Orel <orelvalenti@gmail.com>
2021-01-19 18:17:21 +0100

    Add class Cart and toggleTrigger

--- src/js/script.js
+++ src/js/script.js
@@ -134,7 +134,7 @@
       thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
 
-      initAmountWidget(){
+    initAmountWidget(){
       const thisProduct = this;
 
       thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
@@ -308,20 +308,55 @@
       const thisWidget = this;
 
       thisWidget.input.addEventListener('change', function(){
-        thisWidget.setValue(thisWidget.input.value)
+        thisWidget.setValue(thisWidget.input.value);
       });
       thisWidget.linkDecrease.addEventListener('click', function(event){
         event.preventDefault();
-        thisWidget.setValue(thisWidget.value - 1)
+        thisWidget.setValue(thisWidget.value - 1);
       });
       thisWidget.linkIncrease.addEventListener('click', function(event){
         event.preventDefault();
-        thisWidget.setValue(thisWidget.value + 1)
+        thisWidget.setValue(thisWidget.value + 1);
       });
     }
   }
 
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+      thisCart.getElements(element);
+      console.log('new Cart', thisCart);
+      thisCart.initActions();
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', () => thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive));
+    }
+
+  }
+
   const app = {
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     initMenu: function(){
       const thisApp = this;
       console.log('thisApp.data: ', thisApp.data);
@@ -333,10 +368,11 @@
       }
     },
 
-    initData: function(){
+    initCart: function(){
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart (cartElem);
     },
 
     init: function(){
@@ -349,6 +385,8 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
+
 
     },
   };
